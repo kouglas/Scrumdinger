f2850d2d8de619e2183e161b875e0a5edfd0103b
Kari <kouglas2@gmail.com>
2024-10-14 18:37:16 -0500

    [UPDATE] create state for Dailyscrum value type and bound it to DetailView, DetailEditView, and ScrumsView

--- Scrumdinger/DetailEditView.swift
+++ Scrumdinger/DetailEditView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct DetailEditView: View {
     
-    @State private var scrum = DailyScrum.emptyScrum
+    @Binding var scrum: DailyScrum
     @State private var newAttendeeName = ""
     
     
@@ -59,6 +59,6 @@ struct DetailEditView: View {
 
 struct DetailEditView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailEditView()
+        DetailEditView(scrum: .constant(DailyScrum.sampleData[0]))
     }
 }
