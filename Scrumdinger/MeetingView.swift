3da9734995dcc03283dcbc19bf7b5edf2dde9567
Kari <kouglas2@gmail.com>
2024-10-16 23:53:26 -0500

    [UPDATE] add some modifiers to meeting progress bar zstack

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -45,6 +45,8 @@ struct MeetingView: View {
             
         }
         .padding()
+        .foregroundColor(scrum.theme.accentColor)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
