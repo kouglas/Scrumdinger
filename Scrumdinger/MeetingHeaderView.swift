6580d732766362c26091398f681fca29ee94ba0a
Kari <kouglas2@gmail.com>
2024-10-19 13:35:06 -0500

    [UPDATE] move the meeting header into its own component. Add custom progressview style

---
+++ Scrumdinger/MeetingHeaderView.swift
@@ -0,0 +1,55 @@
+//
+//  MeetingHeaderView.swift
+//  Scrumdinger
+//
+//  Created by Kari Douglas on 10/18/24.
+//
+
+import SwiftUI
+
+struct MeetingHeaderView: View {
+    let secondsElapsed: Int
+    let secondsRemaining: Int
+    let theme: Theme
+    
+    private var totalSeconds: Int {
+        secondsElapsed + secondsRemaining
+    }
+    
+    private var progress: Double {
+        guard totalSeconds > 0 else { return 1 }
+        return Double(secondsElapsed) / Double(totalSeconds)
+    }
+    
+    private var minutesRemaining: Int {
+        secondsRemaining / 60
+        
+    }
+    
+    var body: some View {
+        VStack {
+            ProgressView(value: progress, total:  15)
+                .progressViewStyle(ScrumProgressViewStyle(theme: theme))
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("\(secondsElapsed)", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing){
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("\(secondsRemaining)", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+        }
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel("Time remaining")
+        .accessibilityValue("\(minutesRemaining) minutes")
+    }
+}
+
+#Preview {
+    MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180, theme: .bubblegum)
+}
