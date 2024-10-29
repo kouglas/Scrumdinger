1571c2435f4febf549316ecfc51674e303a99c88
Kari <kouglas2@gmail.com>
2024-10-29 14:58:05 -0500

    [UPDATE] monitor scene phase in app to determine operational state. If the state is inactive user scrum info is saved and remains unchanged on app launh

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ScrumsView: View {
     @Binding var scrums: [DailyScrum]
     @State private var isPresentingNewScrumView = false
+    @Environment(\.scenePhase) private var scenePhase
+    let saveAction: ()-> Void
     
     var body: some View {
         NavigationStack {
@@ -33,11 +35,15 @@ struct ScrumsView: View {
         .sheet(isPresented: $isPresentingNewScrumView) {
             NewScrumSheet(isPresentingNewScrumView: $isPresentingNewScrumView, scrums: $scrums)
         }
+        .onChange(of: scenePhase) { phase in
+            if phase == .inactive { saveAction() }
+            
+        }
     }
 }
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
-        ScrumsView(scrums: .constant(DailyScrum.sampleData))
+        ScrumsView(scrums: .constant(DailyScrum.sampleData), saveAction: {})
     }
 }
